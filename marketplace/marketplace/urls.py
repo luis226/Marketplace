97319e94cbb51d53d30a8d85fb52e8c2fe211bbe
luisgalaviz <luisangeljaja@hotmail.com>
2022-03-23 23:25:09 -0600

    Added swagger

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -15,17 +15,36 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from store.drf.views import ProductViewset, UserView
 
+
 router = DefaultRouter()
 router.register(r'products', ProductViewset, basename='products')
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Marketplace API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token, name='obtain-token'),
     path('users/', UserView.as_view(), name='users'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    url('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
