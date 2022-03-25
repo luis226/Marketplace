e069d1f721bcd015495e20c5aec7cf7d5dd5eda5
luisgalaviz <luisangeljaja@hotmail.com>
2022-03-24 22:34:21 -0600

    Removed test_unit

--- marketplace/store/tests/tests_unit.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class MyTestCase(TestCase):
-    def test_my_test(self):
-        pass
-
-    def test_failing_test(self):
-        self.assertTrue(False)
